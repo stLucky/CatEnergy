88114ff270ca4ffa2c0c082acb1570043eb3eca2
stLucky <asys4759@gmail.com>
2020-12-19 20:23:19 +0300

    Доработки к защите

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,21 +1,24 @@
 const slider = document.querySelector(".slider");
-const slideBefore = slider.querySelector(".slider__item--before");
-const slideAfter = slider.querySelector(".slider__item--after");
-const sliderButtonBefore = slider.querySelector(".slider__button--before");
-const sliderButtonAfter = slider.querySelector(".slider__button--after");
+const slideBefore = document.querySelector(".slider__item--before");
+const slideAfter = document.querySelector(".slider__item--after");
+const sliderBar = document.querySelector(".slider__bar");
+const sliderButtonBefore = document.querySelector(".slider__button--before");
+const sliderButtonAfter = document.querySelector(".slider__button--after");
 
+if (slideBefore && slideAfter && sliderBar && sliderButtonBefore && sliderButtonAfter) {
+  sliderButtonBefore.addEventListener("click", function () {
+    slideBefore.classList.add("slider__item--current");
+    slideAfter.classList.remove("slider__item--current");
+    sliderBar.classList.remove("slider__bar--after");
+    slideBefore.style.width = "100%";
+    slideAfter.style.width = "0";
+  });
 
-
-sliderButtonBefore.addEventListener("click", function () {
-  slideBefore.classList.add("slider__item--current");
-  slideAfter.classList.remove("slider__item--current");
-  slideBefore.style.width = "100%";
-  slideAfter.style.width = "0";
-});
-
-sliderButtonAfter.addEventListener("click", function () {
-  slideAfter.classList.add("slider__item--current");
-  slideBefore.classList.remove("slider__item--current");
-  slideAfter.style.width = "100%";
-  slideBefore.style.width = "0";
-});
+  sliderButtonAfter.addEventListener("click", function () {
+    slideAfter.classList.add("slider__item--current");
+    slideBefore.classList.remove("slider__item--current");
+    sliderBar.classList.add("slider__bar--after");
+    slideAfter.style.width = "100%";
+    slideBefore.style.width = "0";
+  });
+}
